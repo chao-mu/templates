3389f109758a4b4105527a3be584b84f9f27266a
Danimal <danimal@hackpoetic.com>
2024-05-21 17:00:17 -0400

    feat(ts-cli-project): add custom validators

--- ts-cli-project/src/index.ts
+++ ts-cli-project/src/index.ts
@@ -1,28 +1,65 @@
-import fs from "node:fs/promises";
-
-import yargs from "yargs/yargs";
 import { hideBin } from "yargs/helpers";
+import yargs from "yargs/yargs";
+
+import fs from "fs";
+
+function check(checkResults: Record<string, string | null>) {
+  for (const [name, reason] of Object.entries(checkResults)) {
+    if (reason) {
+      return `Invalid value for ${name}: ${reason}`;
+    }
+  }
+
+  return true;
+}
+
+function checkDir(value: string): string | null {
+  if (!fs.existsSync(value)) {
+    return "Expected existing directory, but it does not exist.";
+  }
+
+  const stat = fs.lstatSync(value);
+  if (!stat.isDirectory()) {
+    return "Path does not lead to a directory.";
+  }
+
+  return null;
+}
+
+function checkFile(value: string): string | null {
+  if (!fs.existsSync(value)) {
+    return `Expected existing file, but it does not exist. Got: ${value}`;
+  }
+
+  const stat = fs.lstatSync(value);
+  if (!stat.isFile()) {
+    return `Path does not lead to a file. Got: ${value}`;
+  }
+
+  return null;
+}
 
 yargs(hideBin(process.argv))
   .command(
-    "serve [port]",
-    "start the server",
+    "echo <file>",
+    "Echo a file path of a file that exists.",
     (yargs) => {
-      return yargs.positional("port", {
-        describe: "port to bind on",
-        default: 5000,
-      });
+      return yargs
+        .positional("file", {
+          describe: "The file path to echo",
+          demandOption: true,
+          type: "string",
+        })
+        .check(({ file }) =>
+          check({
+            path: checkFile(file),
+          }),
+        );
     },
-    (argv) => {
-      if (argv.verbose) console.info(`start server on :${argv.port}`);
-      console.log("serving");
+    ({ file }) => {
+      console.log("I'd read this if I wanted to", file);
     },
   )
-  .option("verbose", {
-    alias: "v",
-    type: "boolean",
-    description: "Run with verbose logging",
-  })
   .demandCommand()
   .strict()
   .parse();
