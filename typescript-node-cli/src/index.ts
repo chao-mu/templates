8574fe62e5b966b7dfec5f61a4b0d150766ff9aa
chao-mu <me@queerlyautumn.com>
2024-07-12 16:23:17 -0400

    fix(typescript-node-cli): linting broken

--- typescript-node-cli/src/index.ts
+++ typescript-node-cli/src/index.ts
@@ -5,27 +5,33 @@ import yargs from "yargs/yargs";
 // Ours
 import { check, checkFile } from "./cli-helpers";
 
-await yargs(hideBin(process.argv))
-  .command(
-    "echo <file>",
-    "Echo a file path of a file that exists.",
-    (yargs) => {
-      return yargs
-        .positional("file", {
-          describe: "The file path to echo",
-          demandOption: true,
-          type: "string",
-        })
-        .check(({ file }) =>
-          check({
-            path: checkFile(file),
-          }),
-        );
-    },
-    ({ file }) => {
-      console.log("I'd read this if I wanted to", file);
-    },
-  )
-  .demandCommand()
-  .strict()
-  .parse();
+async function main() {
+  await yargs(hideBin(process.argv))
+    .command(
+      "echo <file>",
+      "Echo a file path of a file that exists.",
+      (yargs) => {
+        return yargs
+          .positional("file", {
+            describe: "The file path to echo",
+            demandOption: true,
+            type: "string",
+          })
+          .check(({ file }) =>
+            check({
+              path: checkFile(file),
+            }),
+          );
+      },
+      ({ file }) => {
+        console.log("I'd read this if I wanted to", file);
+      },
+    )
+    .demandCommand()
+    .strict()
+    .parse();
+}
+
+main().catch((err: unknown) => {
+  console.error(err);
+});
